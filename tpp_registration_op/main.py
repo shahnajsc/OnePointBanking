dcedac28f8a71fad5514e63d739d55590b654eb0
Shahnaj Sharmin Chowdhury <sschowdhury29@.com>
2025-10-15 23:07:23 +0300

    TPP registration JWT added

--- tpp_registration_op/main.py
+++ tpp_registration_op/main.py
@@ -1,8 +1,15 @@
 from scripts.utils import load_env
+from scripts.generate_certs import generate_certs
+from scripts.generate_reg_jwt import generate_reg_jwt
+from scripts.register_tpp import register_tpp
+
 
 def main():
-	env = load_env("conf/env.json")
-print("ENV is ready")
+    print("Starting OP TPP registration flow...")
+    env = load_env("conf/env.json")
+    ssa_data = generate_certs(env)
+    reg_jwt = generate_reg_jwt(env, ssa_data)
+    tpp_respons = register_tpp(env, reg_jwt)
 
 if __name__ == "__main__":
-	main()
+    main()
