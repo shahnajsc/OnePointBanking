1cf42b407072dc87918d5d4add3fdf17accdf278
Shahnaj Sharmin Chowdhury <sschowdhury29@.com>
2025-10-15 23:29:12 +0300

    Utility updated

--- tpp_registration_op/scripts/utils.py
+++ tpp_registration_op/scripts/utils.py
@@ -1,11 +1,7 @@
 import json
-import os
-import requests
+
 
 def load_env(path="conf/env.json"):
-	"""It loads env JSON file"""
-	with open(path, "r") as env_fd:
-		return json.load(env_fd)
-
-
-
+	"""Load environment configuration"""
+	with open(path, "r") as f:
+		return json.load(f)
