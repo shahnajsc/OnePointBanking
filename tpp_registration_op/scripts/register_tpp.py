e2d251b4bfe03a899bd177428cd16ffa028d619d
Shahnaj Sharmin Chowdhury <sschowdhury29@.com>
2025-10-21 14:46:59 +0300

    Validation check added

--- tpp_registration_op/scripts/register_tpp.py
+++ tpp_registration_op/scripts/register_tpp.py
@@ -13,9 +13,28 @@ def register_tpp(env, reg_jwt):
 
 	response = requests.post(env.get("registration_url"), data=reg_jwt, headers=headers, cert=qwac_cert, verify=False)
 
-	print("📡 Status:", response.status_code)
+	if response.status_code != 201:
+		raise SystemExit("❌ TPP registration request failed")
 
-	resp_json = response.json()
-	print(json.dumps(resp_json, indent=2))
+	print("✅ TPP Registration Successful. Proceeding for Validation")
 
-	return response
+	tpp_info = response.json()
+	with open("certs/ttp_info.json", "w") as tpp_write:
+		json.dump(tpp_info, tpp_write, indent=2)
+
+	client_id = tpp_info.get("client_id", None)
+	client_secret = tpp_info.get("client_secret", None)
+
+	data = {
+		"grant_type": "client_credentials",
+		"scope": "accounts",
+		"client_id": tpp_info.get("client_id", None),
+		"client_secret": tpp_info.get("client_secret", None)
+	}
+	token_url = "https://psd2.mtls.sandbox.apis.op.fi/oauth/token"
+
+	token_resp = requests.post(token_url, data=data, cert=qwac_cert, verify=False)
+	if token_resp.status_code != 200:
+		raise SystemExit("❌ TPP validation failed")
+
+	print("✅ TPP Validation Successful.")
